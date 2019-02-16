23b6d155042b74eb429ca752aa86d2ac2b585531
Andreu Vallbona Plazas <avallbona@gmail.com>
2019-02-16 15:13:41 +0100

    more changes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils.timezone import now
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {})
